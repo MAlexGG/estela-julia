d27a46e67a44aee9dac0b2ef4f574dd6bf6be069
MAlexGG <alexandragalarzag@gmail.com>
2024-10-31 21:35:58 +0100

    mediaqueries in home page

--- src/config/Router.jsx
+++ src/config/Router.jsx
@@ -3,8 +3,12 @@ import App from '../App'
 import Videos from "../pages/videos/Videos"
 import Tours from "../pages/tours/Tours"
 import NotFound from "../pages/notFound/NotFound"
+import Dashboard from "../pages/dashboard/Dashboard"
 
 function Router() {
+  
+  const url = import.meta.env.VITE_ADMIN_URL;
+  
   return (
     <BrowserRouter>
         <Routes>
@@ -12,6 +16,7 @@ function Router() {
             <Route path="/videos" element={<Videos/>} />
             <Route path="/tours-events" element={<Tours/>} />
             <Route path="*" element={<NotFound/>} />
+            <Route path={`/${url}`} element={<Dashboard/>}/>
         </Routes>
     </BrowserRouter>
   )
