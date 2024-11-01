4674d6c2da621d6e5aa7ba6da181df7c5207386c
MAlexGG <alexandragalarzag@gmail.com>
2024-11-01 12:39:54 +0100

    hamburguer menu mobile complete

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -1,19 +1,26 @@
 import { NavLink } from "react-router-dom"
 import styles from './Navbar.module.css'
 import menu from '../../assets/images/menu.svg'
+import { useState } from "react";
 
 function Navbar() {
+
+  const [isOpen, setIsOpen] = useState(false);
+
+  const handleClick = () => {
+    setIsOpen(!isOpen);
+  }
 
   const contactEmail = import.meta.env.VITE_CONTACT_EMAIL;
 
   return (
-    <header className={styles.ctHeader}>
-      <button className={styles.btMenu}>
+    <header className={isOpen ? styles.ctHeader : styles.ctHeaderClose}>
+      <button className={styles.btMenu} onClick={handleClick}>
         <img src={menu} alt="hamburguer menu" className={styles.imgMenu} />
       </button>
-        <nav className={styles.ctNavbar}>
+        <nav className={isOpen ? styles.ctNavbar : styles.ctNavbarClose}>
             <NavLink to={"/"} className={({isActive}) => isActive ? styles.navActive : ""}>INICIO</NavLink>
-            <a href="/#discografia">DISCOGRAFÍA</a>
+            <a href="/#discografia"  onClick={handleClick}>DISCOGRAFÍA</a>
             {/* <NavLink to={"#discografia"}>DISCOGRAFÍA</NavLink> */}
             <NavLink to={"/videos"} className={({isActive}) => isActive ? styles.navActive : ""}>VIDEOS</NavLink>
             <NavLink to={"/tours-events"} className={({isActive}) => isActive ? styles.navActive : ""}>GIRAS Y EVENTOS</NavLink>
