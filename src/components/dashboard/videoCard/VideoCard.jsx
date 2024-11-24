6393a3d650cba9cabe17e4957b1ecec538c7320a
MAlexGG <alexandragalarzag@gmail.com>
2024-11-24 16:52:55 +0100

    delete alert in video admin page

--- src/components/dashboard/videoCard/VideoCard.jsx
+++ src/components/dashboard/videoCard/VideoCard.jsx
@@ -1,14 +1,29 @@
 import styles from './VideoCard.module.css'
 import deleteIcon from '../../../assets/images/delete.svg'
 import Video from '../../video/Video'
+import Alert from '../../alert/Alert'
+import { useState } from 'react';
 
 
 function VideoCard({video, handleDelete}) {
 
+  const [alert, setAlert] = useState(false);
+
+    const handleOpenDelete = () => {
+        setAlert(true)
+    };
+      
+    const handleCancelDelete = () => {
+    setAlert(false);
+    };
+
   return (
     < div className={styles.ctVideoCard}>
       <Video video={video}/>
-      <button className={styles.btDelete} onClick={()=>handleDelete(video.id)}><img src={deleteIcon} alt="delete button" /></button>
+      <button className={styles.btDelete} onClick={handleOpenDelete}><img src={deleteIcon} alt="delete button" /></button>
+      {
+        alert && <Alert message={'¿Estás segura de que quieres eliminar el video?'} state={alert ? 'open' : 'close'} handleCancelDelete={handleCancelDelete} handleAcceptDelete={()=>handleDelete(video.id)}/>
+      }
     </div>
   )
 }
